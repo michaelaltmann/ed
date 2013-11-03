caafe86103053d685611b312c088353203a0eaee
Michael Altmann <michael.altmann@gmail.com>
2013-11-03 09:22:09 -0600

    Cloropleth of hosp quality on top of map

--- client/ed.js
+++ client/ed.js
@@ -5,6 +5,7 @@ Template.hello.greeting = function () {
 Template.hello.events({
     'click input': function () {
         // template data, if any, is available in 'this'
+        loadMap();
         loadPoints();
         drawDiagram();
     }
@@ -12,55 +13,67 @@ Template.hello.events({
 
 Meteor.subscribe("hospitals");
 
-var width = 960,
+var width = 600,
     height = 500,
-    minlat = 42.3,
-    maxlat = 47.5,
-    minlng = -96.3,
-    maxlng = -91.1,
-    maxRate = 200;
+    maxRate = 60;
 
-var colors = [
-    "rgb(197,27,125)",
-    "rgb(222,119,174)",
-    "rgb(241,182,218)",
-    "rgb(253,224,239)",
-    "rgb(247,247,247)",
-    "rgb(230,245,208)",
-    "rgb(184,225,134)",
-    "rgb(127,188,65)",
-    "rgb(77,146,33)"
-];
+var bBox = [[43.5, -97],[49, -91]];
 
-
-var voronoi = d3.geom.voronoi();
 
 var canvas;
 var context;
 var sites, siteMap;
+var svg;
 
-function scale(pt) {
-    x = width * (pt[1] - minlng) / (maxlng - minlng);
-    if (x < 0) x = 0;
-    if (x > width) x = width;
-    y = height * (pt[0] - minlat) / (maxlat - minlat);
-    if (y < 0) y = 0;
-    if (y > height) y = height;
-    return [x, y];
+var projection;
+var voronoi;
+
+function init() {
+    projection = d3.geo.albers()
+        .center([0, 46])
+        .rotate([94, 0])
+        .parallels([43, 49])
+        .scale(5000)
+        .translate([width / 2, height / 2]);
+    voronoi = d3.geom.voronoi();
+    voronoi.clipExtent(bBox);
 }
+Meteor.startup(init);
 
 function readOnePoint(hospital) {
-    var screenPt = scale([hospital.lat, hospital.lng]);
-    if (screenPt != null && screenPt.length == 2) {
-        if (siteMap[screenPt] != null) {
-            alert("Skipping dup: " + screenPt);
+    var pt = [hospital.lat, hospital.lng];
+    if (pt != null && pt.length == 2) {
+        if (siteMap[pt] != null) {
+            alert("Skipping dup: " + pt);
         } else {
-            var red = hospital.rate * 255 / maxRate;
-            hospital.style = "rgb(" + red + ",0,0)";     
-            siteMap[screenPt] = hospital;
-            sites[sites.length] = screenPt;
+            siteMap[pt] = hospital;
+            sites[sites.length] = pt;
         }
     }
+}
+
+function createSvg() {
+    if (svg == null) {
+        svg = d3.select("body").append("svg")
+            .attr("overflow", "hidden")
+            .attr("width", width)
+            .attr("height", height);
+    }
+}
+
+function loadMap() {
+    createSvg();
+    d3.json("example-data/mn-county-2010.geo.json", function (error, mn) {
+        var path = d3.geo.path()
+            .projection(projection);
+        svg.append("path")
+            .datum({
+                type: "FeatureCollection",
+                features: mn.features
+            })
+            .attr("class", "map")
+            .attr("d", path);
+    });
 }
 
 function loadPoints() {
@@ -72,51 +85,56 @@ function loadPoints() {
 
 
 function drawDiagram() {
-    canvas = d3.select("body").append("canvas")
-        .attr("width", width)
-        .attr("height", height);
-
-    context = canvas.node().getContext("2d");
-
     redraw();
 }
 
 function redraw() {
+    createSvg();
     var cells = voronoi(sites);
 
-    context.clearRect(0, 0, width, height);
 
     for (var i = 0, n = cells.length; i < n; ++i) {
         var cell = cells[i];
         var screenPt = cell.point;
         hospital = siteMap[screenPt];
-        context.fillStyle = hospital.style;
-        if (draw(cell)) context.fill();
-    }
-
-
-    context.strokeStyle = "white";
-    for (var i = 0, n = cells.length; i < n; ++i) {
-        if (draw(cells[i])) context.stroke();
-    }
-
-    context.fillStyle = "black";
-    for (var i = 1, n = sites.length, site; i < n; ++i) {
-        site = sites[i];
-        context.beginPath();
-        context.arc(site[0], site[1], 1.5, 0, 2 * Math.PI, false);
-        context.fill();
+        console.log("rate " + hospital.rate);
+        var level = Math.floor(hospital.rate * 16 / maxRate);
+        if (level < 0) level = 0;
+        if (level > 15) level = 15;
+        var style = "cell" + level;
+        draw(cell, style);
     }
 }
 
-function draw(cell) {
-    if (cell) {
-        context.beginPath();
-        context.moveTo(cell[0][0], cell[0][1]);
-        for (var j = 1, m = cell.length; j < m; ++j) {
-            context.lineTo(cell[j][0], cell[j][1]);
-        }
-        context.closePath();
-        return true;
+function cellToFeature(cell) {
+    var coordinates = [];
+    console.log("----");
+    // Need to reverse the order because 
+    // D3 geom wants them in clockwise order
+    for (var j = cell.length - 1; j >= 0; j--) {
+        var lat = cell[j][1],
+            lng = cell[j][0];
+        var point = [lat, lng];
+        console.log(point);
+        coordinates[coordinates.length] = point;
     }
+    var feature = {
+        "type": "Feature",
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [coordinates]
+        },
+        "properties": {}
+    }
+    return feature;
+}
+
+function draw(cell, style) {
+    var feature = cellToFeature(cell);
+    var path = d3.geo.path()
+        .projection(projection);
+    svg.append("path")
+        .datum(feature)
+        .attr("class", style)
+        .attr("d", path);
 }
